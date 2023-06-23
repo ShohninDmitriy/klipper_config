72ac73f63a0b0651b1872bf05e500aabc4d6d7e2
Diman <demian77neo@mail.ru>
2023-06-23 18:00:46 +0300

    Auto-commit

--- script/update_mcu.sh
+++ script/update_mcu.sh
@@ -7,7 +7,7 @@ cd ~/klipper
 echo "Start update mcu octopus"
 echo ""
 make clean
-#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.octopus_mcu
+make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.octopus_mcu
 make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.octopus_mcu
 #read -p "mcu octopus firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.octopus_mcu FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_10011-if00
@@ -18,7 +18,7 @@ echo ""
 echo "Start update mcu stm32f103"
 echo ""
 make clean
-#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.stm32f103_mcu
+make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.stm32f103_mcu
 make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.stm32f103_mcu
 #read -p "mcu stm32f103 firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.stm32f103_mcu FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f103xe_10012-if00
@@ -29,7 +29,7 @@ echo ""
 echo "Start update mcu SHT42"
 echo ""
 make clean
-#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.SHT42_mcu
+make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.SHT42_mcu
 make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.SHT42_mcu
 #read -p "mcu SHT42 firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.SHT42_mcu FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f072xb_10013-if00
